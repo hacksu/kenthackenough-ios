7dc42474b5c467e4293143ed51374131cc3d32c0
Wes Delp <wesdelp@me.com>
2015-09-17 13:57:28 -0400

    Tab bar and Views Working
    - Fixed initial view to point to Dash
    - Tabs now point to view files
    - Notification badge for unread updates is defined at start-up and is
    cleared when updates tab is selected

--- KHE/schedule.js
+++ KHE/schedule.js
@@ -1,10 +1,38 @@
 'use strict';
 
 var React = require('react-native');
+
 var {
-  AppRegistry,
   StyleSheet,
-  TabBarIOS,
   Text,
   View,
+  Component
 } = React;
+
+class Schedule extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      <Text style={styles.description}>
+      Welcome to the SCHEDULE
+      </Text>
+      </View>
+    )
+  }
+}
+
+var styles = StyleSheet.create({
+  description: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#FFFFFF'
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#231F20'
+  }
+})
+
+module.exports = Schedule

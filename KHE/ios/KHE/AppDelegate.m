89f45fcbf6f8545bc2fbbcbce0133d6540fd099d
Wes Delp <wesdelp@me.com>
2015-09-17 23:27:42 -0400

    merging

--- KHE/ios/KHE/AppDelegate.m
+++ KHE/ios/KHE/AppDelegate.m
@@ -31,7 +31,13 @@
    * on the same Wi-Fi network.
    */
 
+<<<<<<< Updated upstream
    jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle"];
+=======
+
+  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle"];
+
+>>>>>>> Stashed changes
 
   /**
    * OPTION 2

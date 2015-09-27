49d7d1d9ffb000d52ab9a5181c2c31c2316fce2b
Wes Delp <wesdelp@me.com>
2015-09-27 14:13:42 -0400

    Dash view now supports scaling for all iPhone models

--- KHE/dash.js
+++ KHE/dash.js
@@ -7,7 +7,7 @@ var {
   Text,
   View,
   Component,
-  Image
+  Image,
 } = React;
 
 class Dash extends Component {
@@ -26,17 +26,15 @@ class Dash extends Component {
         style={styles.logoImage}>
         </Image>
 
-        <Text style={styles.description}>
-        Welcome to Kent Hack Enough!
-        </Text>
+
 
         <Text style={styles.dashHeader}>Latest Update!</Text>
-        <Text style={styles.dashBox}>
+        <Text style={styles.updatesDashBox}>
         {this.state.newUpdate}
         </Text>
 
         <Text style={styles.dashHeader}>Next Event!</Text>
-        <Text style={styles.dashBox}>
+        <Text style={styles.scheduleDashBox}>
         {this.state.nextEvent}
         </Text>
       </View>
@@ -46,41 +44,57 @@ class Dash extends Component {
 
 var styles = StyleSheet.create({
   description: {
+    flex: 1,
     marginTop: 10,
     fontSize: 14.5,
+    width: 300,
     textAlign: 'center',
     color: '#FFFFFF'
   },
-  dashBox: {
-    //flex: 1,
-    fontSize: 14.5,
-    backgroundColor: 'red',
-    height: 80,
+  updatesDashBox: {
+    flex: 1,
+    fontSize: 13,
+    backgroundColor: 'white',
+    height: 100,
     width: 250,
     textAlign: 'left',
-    color: '#FFFFFF'
+    color: 'black',
+    position: 'relative',
+  },
+  scheduleDashBox: {
+    flex: 1,
+    fontSize: 13,
+    backgroundColor: 'white',
+    height: 100,
+    width: 250,
+    marginBottom: 80,
+    textAlign: 'left',
+    color: 'black',
+    position: 'relative',
   },
   dashHeader: {
-    marginTop: 50,
-    fontSize: 14.5,
-    backgroundColor: 'white',
+    marginTop: 40,
+    fontSize: 13,
+    backgroundColor: 'black',
     width: 250,
     textAlign: 'center',
-    color: 'black',
+    color: 'white',
     fontWeight: 'bold'
   },
   container: {
     flex: 1,
+    position: 'relative',
     alignItems: 'center',
     backgroundColor: '#231F20',
-    paddingVertical: 20
+    paddingVertical: 20,
   },
   logoImage: {
-    flex:0,
-    width: 300,
+    flex: 1,
+    width: 250,
     height: 100,
-    marginTop: 80,
-    resizeMode: Image.resizeMode.contain
+    marginTop: 40,
+    resizeMode: Image.resizeMode.contain,
+    position: 'relative',
   }
 })
 
